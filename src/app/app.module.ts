60b7bf3e5c7d9e954e17c86e4a8d7f56adb8f403
ikalekarson <sonalikalekar95@gmail.com>
2024-04-02 14:54:37 +0530

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ItemCartComponent } from './components/item-cart/item-cart.component';
+import { ItemDashboardComponent } from './components/item-dashboard/item-dashboard.component';
+import { ItemDetailsComponent } from './components/item-details/item-details.component';
+import { HeaderComponent } from './shared/template/header/header.component';
+import { CartService } from './services/cart.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemDashboardComponent,
+    ItemDetailsComponent,
+    ItemCartComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [CartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
